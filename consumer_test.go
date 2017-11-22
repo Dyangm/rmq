54e71627df77a322604b4499c75e2c06428ed336
hulkwo <hulk.wo@gmail.com>
2017-11-22 11:53:19 +0800

    加入consumer测试

---
+++ consumer_test.go
@@ -0,0 +1,22 @@
+package rmq
+
+import "testing"
+
+func TestNewConsumer(t *testing.T) {
+	uri := "amqp://udc:123456@192.168.10.53:5672//udc_host"
+	queue := "go_real_data_queue"
+	c, err := NewConsumer(uri, queue, "rmqtest")
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	defer c.Shutdown()
+
+	msg := <- c.MsgChan
+	if msg != nil {
+		return
+	}
+
+	t.Fail()
+}
